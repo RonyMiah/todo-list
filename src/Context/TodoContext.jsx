8a932f734ab32d676db80a943fe25867ac99e9ea
Rony <rony@Ronys-Air.W4>
2023-10-14 22:32:41 +0600

    context added

--- src/Context/TodoContext.jsx
+++ src/Context/TodoContext.jsx
@@ -1,10 +1,34 @@
-import { createContext } from 'react';
+import { createContext, useState } from 'react';
 
 export const TodoContext = createContext();
 
 // eslint-disable-next-line react/prop-types
 export default function TodoProvider({ children }) {
-  const value = {};
+  const [todos, setTodos] = useState([]);
+
+  // save todo object
+  const onSave = (data) => {
+    const allTodos = [...todos];
+    allTodos.push(data);
+    localStorage.setItem('todos', JSON.stringify(allTodos));
+    setTodos(allTodos);
+  };
+
+  // update a todo only takes the todo object and the index of the todo
+
+  const onUpdate = (data, index) => {
+    const allTodos = [...todos];
+    const obj = { ...allTodos[index], ...data };
+    allTodos.splice(index, 1, obj);
+    localStorage.setItem('todos', JSON.stringify(allTodos));
+    setTodos(allTodos);
+  };
+
+  const value = {
+    todos,
+    onSave,
+    onUpdate,
+  };
 
   return <TodoContext.Provider value={value}>{children}</TodoContext.Provider>;
 }
