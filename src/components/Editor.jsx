47e033f723e76fa3d0e8e03098fa90ee78fd867e
Rony <rony@Ronys-Air.W4>
2023-10-14 23:27:36 +0600

    Editor npm packge use for edit

---
+++ src/components/Editor.jsx
@@ -0,0 +1,20 @@
+import { BlockNoteView, useBlockNote } from '@blocknote/react';
+import '@blocknote/core/style.css';
+import propTypes from 'prop-types';
+
+export default function Editor({ initialContent, setContent }) {
+  const editor = useBlockNote({
+    initialContent: initialContent ? JSON.parse(initialContent) : undefined,
+    onEditorContentChange: (editor) => {
+      const blocks = editor.topLevelBlocks;
+
+      console.log('Content was changed : ', blocks);
+    },
+  });
+  return <BlockNoteView editor={editor} theme={'light'} />;
+}
+
+Editor.propTypes = {
+  initialContent: propTypes.object,
+  setContent: propTypes.func,
+};
